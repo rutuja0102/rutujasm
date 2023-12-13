a0f30a3cc18b78a59a1d61239042d177df4d986e
varad4545 <godvarad@gmail.com>
2023-12-13 17:05:55 +0530

    added:formatting:config-changes

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: "postgres",
-    password: "pass@271",
-    database: "db_seva",
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASENAME,
     host: "127.0.0.1",
     dialect: "postgres"
   },
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,93 +1,93 @@
-const express = require("express")
-const router= express.Router();
-const sevas= require("../../database/models").seva;
+const express = require("express");
+const router = express.Router();
+const sevas = require("../../database/models").seva;
 
-router.post("/createBasicSevaInfo",async(req, res)=>{
+router.post("/createBasicSevaInfo", async (req, res) => {
   try {
-    const {sevaName,sevaDescription,sevaPrice,sevaScheduleType} = req.body;
+    const { sevaName, sevaDescription, sevaPrice, sevaScheduleType } = req.body;
     const existingSeva = await sevas.findOne({ where: { name: sevaName } });
     if (existingSeva) {
-        return res.status(500).json("Seva already exists");
+      return res.status(500).json("Seva already exists");
     }
     const requiredFields = {
-    sevaName: 'Name is required',
-    sevaDescription: 'Description is required',
-    sevaPrice: 'Seva price is required',
-    sevaScheduleType: 'Seva schedule type is required'
-  };
-  for (const field in requiredFields) {
-    if (!req.body[field]) {
-      return res.status(400).json({ message: requiredFields[field] });
+      sevaName: "Name is required",
+      sevaDescription: "Description is required",
+      sevaPrice: "Seva price is required",
+      sevaScheduleType: "Seva schedule type is required",
+    };
+    for (const field in requiredFields) {
+      if (!req.body[field]) {
+        return res.status(400).json({ message: requiredFields[field] });
+      }
     }
-  }
-  const newSeva = await sevas.create({
+    const newSeva = await sevas.create({
       name: sevaName,
       description: sevaDescription,
       price: sevaPrice,
-      sevaScheduleType: sevaScheduleType
-    })
-    res.status(201).json({ message: 'Seva created successfully'});
-   } catch (error) { 
+      sevaScheduleType: sevaScheduleType,
+    });
+    res.status(201).json({ message: "Seva created successfully" });
+  } catch (error) {
     return res.status(500).json(error);
-}
+  }
 });
 
 router.post("/updateSeva/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const {fixedScheduleType,fixedDaysData} = req.body; 
-        console.log("BOdy: ", fixedScheduleType);
-      const foundSeva = await sevas.findByPk(id);
-      if (!foundSeva) {
-        return res.status(404).json({ message: 'Seva not found' });
-      }
-      foundSeva.fixedScheduleType = fixedScheduleType;
-      foundSeva.fixedDaysData = fixedDaysData;
-      await sevas.update(
-        foundSeva
-        
-       , {
-        where : {
-            id
-        }
-       });
-        await foundSeva.save();
-       res.status(200).json({ message: 'Seva updated successfully', updatedSeva: foundSeva });
-        } catch (error) {
-      
-      res.status(500).json({ message: 'Error updating Seva' });
+  try {
+    const { id } = req.params;
+    const { fixedScheduleType, fixedDaysData } = req.body;
+    console.log("BOdy: ", fixedScheduleType);
+    const foundSeva = await sevas.findByPk(id);
+    if (!foundSeva) {
+      return res.status(404).json({ message: "Seva not found" });
     }
-  });
+    foundSeva.fixedScheduleType = fixedScheduleType;
+    foundSeva.fixedDaysData = fixedDaysData;
+    await sevas.update(
+      foundSeva,
 
-  router.post("/updateVariableSevaCalendar/:id", async (req, res) => {
-    try {
-      const { id } = req.params;
-      const { variableDatesData } = req.body;
-      console.log(variableDatesData)
-      const foundSeva = await sevas.findByPk(id);
-      if (!foundSeva) {
-        return res.status(404).json({ message: 'Seva not found' });
+      {
+        where: {
+          id,
+        },
       }
-  
-      foundSeva.variableDatesData = variableDatesData;
-  
-      await sevas.update(
-        foundSeva
-       , {
-        where : {
-            id
-        }
-       });
+    );
+    await foundSeva.save();
+    res
+      .status(200)
+      .json({ message: "Seva updated successfully", updatedSeva: foundSeva });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating Seva" });
+  }
+});
 
-       await foundSeva.save();
-       
-      res.status(200).json({ message: 'Seva updated successfully', updatedSeva: foundSeva });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error updating Seva' });
+router.post("/updateVariableSevaCalendar/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { variableDatesData } = req.body;
+    console.log(variableDatesData);
+    const foundSeva = await sevas.findByPk(id);
+    if (!foundSeva) {
+      return res.status(404).json({ message: "Seva not found" });
     }
-  });
-  
-  module.exports = router;
 
-  
+    foundSeva.variableDatesData = variableDatesData;
+
+    await sevas.update(foundSeva, {
+      where: {
+        id,
+      },
+    });
+
+    await foundSeva.save();
+
+    res
+      .status(200)
+      .json({ message: "Seva updated successfully", updatedSeva: foundSeva });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error updating Seva" });
+  }
+});
+
+module.exports = router;
